bd0de66b0d39a6c1b186afbd9413e90f6c8ad2fc
meghapai <paimegha99@gmail.com>
2024-07-03 15:11:47 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6850d90f-5cf8-4226-b16e-1dde5751b5c7';
+export const MODAL_CLOSE_SEC = 2.5;
